21efb3f082291265544adbc4b0ba0a1d3dc4b841
bgoncharuck <dariydragon26@gmail.com>
2019-09-02 23:26:06 +0300

    prices button

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Prices Button
+
+function showPricesWindow() {
+  let pricesWindow = document.getElementById("pricesWindow");
+  pricesWindow.style.setProperty("display", "block");
+}
+
+function closePricesWindow() {
+  let pricesWindow = document.getElementById("pricesWindow");
+  pricesWindow.style.setProperty("display", "none");
+}
