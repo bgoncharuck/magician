f3d0653333db347204a7a6a89397764c815e7885
bgoncharuck <dariydragon26@gmail.com>
2019-09-03 13:14:00 +0300

    dropdown lang

--- scripts.js
+++ scripts.js
@@ -1,12 +1,45 @@
 // Prices Window
 function showPricesWindow() {
-  let pricesWindow = document.getElementById("pricesWindow");
-  pricesWindow.style.setProperty("display", "block");
+  let pricesWindow_el = document.getElementById("pricesWindow");
+  pricesWindow_el.style.setProperty("display", "block");
 }
 
 function closePricesWindow() {
-  let pricesWindow = document.getElementById("pricesWindow");
-  pricesWindow.style.setProperty("display", "none");
+  let pricesWindow_el = document.getElementById("pricesWindow");
+  pricesWindow_el.style.setProperty("display", "none");
 }
 
 // Options Buttons
+
+// Language Button
+function showLanguageOptions() {
+  let languageDropDownMenu_el = document.getElementById("languageDropDownMenu");
+  languageDropDownMenu_el.style.setProperty("display", "flex");
+  let languageButton_el = document.getElementById("languageButton");
+  languageButton_el.style.setProperty("visibility", "hidden");
+}
+
+function languageDropDownMenuClicked() {
+  let languageDropDownMenu_el = document.getElementById("languageDropDownMenu");
+  languageDropDownMenu_el.style.setProperty("display", "none");
+  let languageButton_el = document.getElementById("languageButton");
+  languageButton_el.style.setProperty("visibility", "visible");
+}
+
+// Language Choise
+
+function languageUkranianClicked() {
+  languageDropDownMenuClicked();
+}
+
+function languageEnglishClicked() {
+  languageDropDownMenuClicked();
+}
+
+function languageGermanClicked() {
+  languageDropDownMenuClicked();
+}
+
+function languageRussianClicked() {
+  languageDropDownMenuClicked();
+}
