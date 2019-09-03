05891d3dec47690ce90f8c7161834138a1f254f9
bgoncharuck <dariydragon26@gmail.com>
2019-09-03 14:53:17 +0300

    optionInfoToggle

--- scripts.js
+++ scripts.js
@@ -10,6 +10,47 @@ function closePricesWindow() {
 }
 
 // Options Buttons
+function consultationOption() {
+  let consultationInformation_el = document.getElementById("consultationInformation");
+  let isDisplayed_prop = consultationInformation_el.style.getPropertyValue("display");
+  if (isDisplayed_prop !== "block") {
+    consultationInformation_el.style.setProperty("display", "block");
+    let consultationButton_el = document.getElementById("consultationButton");
+    consultationButton_el.classList.toggle("optionButtonToggled");
+  } else if (isDisplayed_prop === "block") {
+    consultationInformation_el.style.setProperty("display", "none");
+    let consultationButton_el = document.getElementById("consultationButton");
+    consultationButton_el.classList.remove("optionButtonToggled");
+  }
+}
+
+function remoteAccessOption() {
+  let remoteAccessInformation_el = document.getElementById("remoteAccessInformation");
+  let isDisplayed_prop = remoteAccessInformation_el.style.getPropertyValue("display");
+  if (isDisplayed_prop !== "block") {
+    remoteAccessInformation_el.style.setProperty("display", "block");
+    let remoteAccessButton_el = document.getElementById("remoteAccessButton");
+    remoteAccessButton_el.classList.toggle("optionButtonToggled");
+  } else if (isDisplayed_prop === "block") {
+    remoteAccessInformation_el.style.setProperty("display", "none");
+    let remoteAccessButton_el = document.getElementById("remoteAccessButton");
+    remoteAccessButton_el.classList.remove("optionButtonToggled");
+  }
+}
+
+function departureOption() {
+  let departureInformation_el = document.getElementById("departureInformation");
+  let isDisplayed_prop = departureInformation_el.style.getPropertyValue("display");
+  if (isDisplayed_prop !== "block") {
+    departureInformation_el.style.setProperty("display", "block");
+    let departureButton_el = document.getElementById("departureButton");
+    departureButton_el.classList.toggle("optionButtonToggled");
+  } else if (isDisplayed_prop === "block") {
+    departureInformation_el.style.setProperty("display", "none");
+    let departureButton_el = document.getElementById("departureButton");
+    departureButton_el.classList.remove("optionButtonToggled");
+  }
+}
 
 // Language Button
 function showLanguageOptions() {
